3c7e49bffebbb78a9dcf7b7ffe81bee3d5a63688
shafa-zeynalli <shafa.zeynalli@outlook.com>
2023-03-22 20:01:30 +0400

    removed comment from app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
 import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
-// import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
